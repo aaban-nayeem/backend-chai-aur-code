1cc72073ede6f88243b0c25ff8a73c1d54bd9249
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-16 18:02:34 +0600

    Video model schema is ready

--- src/index.js
+++ src/index.js
@@ -2,21 +2,12 @@ import connectDB from "./db/index.js";
 import dotenv from "dotenv";
 dotenv.config({ path: "./env" });
 
-connectDB();
-
-// (async () => {
-//   try {
-//     await mongoose.connect(`${process.env.MONGO_URI}`);
-//     app.on("Error", () => {
-//       console.log("Try the Error", error);
-//       throw error;
-//     });
-
-//     app.listen(process.env.PORT, () => {
-//       console.log(`App is listening on port ${process.env.PORT}`);
-//     });
-//   } catch (error) {
-//     console.error("Catch the Error", error);
-//     throw error;
-//   }
-// })();
+connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`Listening on port ${process.env.PORT || 3000}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MongoDB connection failed with error: ${error}`);
+  });
---
+++ src/models/video.model.js
@@ -0,0 +1,56 @@
+import mongoose, { Schema, SchemaType } from "mongoose";
+
+const useSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    fullname: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String,
+      required: true,
+    },
+    coverImage: {
+      type: String,
+    },
+    watchHistory: [
+      {
+        typr: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refreshToken: {
+      type: String,
+    },
+  },
+  { timestamps: true },
+);
+
+
+
+
+
+
+
+export const User = mongoose.model("User", useSchema);
