e3cdd3c324f506b4eefb52c90d541dff01072611
Aaban Nayeem <aabannayeem4358@gmail.com>
2024-06-25 10:42:56 +0600

    verifyJWT middleware is done

--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -1,5 +1,8 @@
+import User from "../models/user.model";
 import { ApiError } from "../utils/ApiError.utils";
 import { asyncHandler } from "../utils/asyncHandler.utils";
+
+import jwt from "jsonwebtoken";
 
 const verifyJWT = asyncHandler(async (req, res, next) => {
   const token =
@@ -9,4 +12,17 @@ const verifyJWT = asyncHandler(async (req, res, next) => {
   if (!token) {
     throw new ApiError(401, "Unauthorized token");
   }
+
+  const decodedToken = jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+  const user = await User.findById(decodedToken._id).select(
+    "-password -refreshToken",
+  );
+
+  if (!user) {
+    throw new ApiError(401, "Unauthorized user");
+  }
+
+  req.user = user;
+  next();
 });
